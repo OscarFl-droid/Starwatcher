813714d5ee67d22340f79dc083d35aa946baeb42
OscarFl-droid <mungoutonbeans@hotmail.com>
2026-08-07 16:56:21 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE='starwatcher-v4-1';
+const CORE=['./','./index.html','./styles.css','./app.js','./orbit-worker.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.pathname.endsWith('/data/catalogue.json')){e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)));return}e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(e.request.method==='GET'&&u.origin===location.origin){const cc=r.clone();caches.open(CACHE).then(x=>x.put(e.request,cc))}return r})))});
+self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.matchAll({type:'window',includeUncontrolled:true}).then(cs=>cs[0]?cs[0].focus():clients.openWindow('./')))});
